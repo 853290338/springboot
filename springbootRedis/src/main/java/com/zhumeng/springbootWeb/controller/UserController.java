71310f90e1839f505198071e74ff7e0e8b528d4e
853290338 <32835749+853290338@users.noreply.github.com>
2017-12-13 15:14:40 +0800

    测试session

--- springbootRedis/src/main/java/com/zhumeng/springbootWeb/controller/UserController.java
+++ springbootRedis/src/main/java/com/zhumeng/springbootWeb/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.zhumeng.springbootWeb.controller;
 
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +20,21 @@ public class UserController {
     private UserDao userDao;
 
     @RequestMapping("/getUser")
-    @Cacheable(value = "user")
+    @Cacheable(value = "user-key")
     public User getUser() {
 
         User user = userDao.findByAddress("sadasdasd");
         System.out.println("sssssss");
         return user;
+    }
+
+    @RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
     }
 }
