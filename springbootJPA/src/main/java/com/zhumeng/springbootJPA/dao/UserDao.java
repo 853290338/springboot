ac946dd33c63e6a94ec08668a03d9bae7a532b94
853290338 <32835749+853290338@users.noreply.github.com>
2017-12-14 10:18:59 +0800

    # WARNING: pojo dao
    redis springboot

---
+++ springbootJPA/src/main/java/com/zhumeng/springbootJPA/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.zhumeng.springbootJPA.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.zhumeng.springbootJPA.pojo.User;
+
+public interface UserDao extends JpaRepository<User, String> {
+
+    @Query("select u from User u where u.address = ?")
+    User findByAddress(String arrdess);
+
+    User findByName(String name);
+}
