2b3137591033a1910dfb61b87f07dea122e91c46
853290338 <32835749+853290338@users.noreply.github.com>
2017-12-14 10:23:57 +0800

    dao

--- springbootJPA/src/main/java/com/zhumeng/springbootJPA/dao/UserDao.java
+++ springbootJPA/src/main/java/com/zhumeng/springbootJPA/dao/UserDao.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.zhumeng.springbootJPA.pojo.User;
 
+/**
+ * 
+ * */
 public interface UserDao extends JpaRepository<User, String> {
 
     @Query("select u from User u where u.address = ?")
